d9ccaebc738ea852f202271591fe03f88b5e3e56
xavtry <sebastianishot68@gmail.com>
2025-10-20 16:47:43 -0400

    Implement dynamic URL detection utility function
    Added utility function to detect dynamic URLs based on predefined host hints.

--- proxy/utils.js
+++ proxy/utils.js
@@ -1,1 +1,33 @@
+/**
+ * utils.js
+ * Utility helpers: dynamic detection, safe URL parse, etc.
+ */
 
+const url = require("url");
+
+/**
+ * Quick heuristic to decide if we should use Puppeteer.
+ * Expand this list with domains you know are heavy.
+ */
+const dynamicHostHints = [
+  "coolmathgames",
+  "class-77",
+  "itch.io",
+  "roblox",
+  "miniclip",
+  "kongregate",
+  "armorgames",
+  "scirra"
+];
+
+function isLikelyDynamic(targetUrl) {
+  try {
+    const u = new URL(targetUrl);
+    const hostname = u.hostname.toLowerCase();
+    return dynamicHostHints.some(h => hostname.includes(h));
+  } catch (err) {
+    return false;
+  }
+}
+
+module.exports = { isLikelyDynamic };
